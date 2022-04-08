f8f4f4ad5650ce10f9c15b4eefdfc65cebb019c0
Kleindal <kleindalsama@gmail.com>
2022-04-08 12:45:25 +0200

    Initial commit

---
+++ src/views/MainView.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+<div style="display:flex; justify-content:center;">
+    <div class="home-connect">
+      <div class="presentation">
+        <img src="../assets/logo.svg" class="App-logo" alt="logo" />
+        <div class="title-hype">
+          <div>Connectez-vous dès</div>
+          <div>à présent pour </div>
+          <div>participer à l"espace</div>
+          <div>discussions</div>
+        </div>
+      </div>
+      <nav class="connect-page">
+          <div class="login">
+            <RouterLink to="/login">Connexion</RouterLink>
+          </div>
+          <div class="signup">
+            <RouterLink to="/signup">S'inscrire</RouterLink>
+          </div>
+          <div class="chatspace">
+            <RouterLink to="/chatspace">- interface -</RouterLink>
+          </div>
+      </nav>
+    </div>
+    <div class="fix-footer">
+      <footer>
+        <p>@Groupomania</p>
+      </footer>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+</style>
